d3ee72e2ceeca87251df79f013056e33f088f573
Julien Thierry <julienthierrypro@gmail.com>
2022-09-13 13:18:06 +0200

    v1.0

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="./logo192.png" alt="logo react flag" />
+      <h3>React Flag</h3>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/pages/About.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus enim
+        aperiam, delectus ducimus alias magni nulla, eum perspiciatis excepturi
+        corrupti deserunt, sint commodi quis. Aliquam amet totam praesentium
+        quae! Alias natus repudiandae esse nesciunt quia tempora ipsa, quisquam
+        a, earum modi placeat omnis doloremque! Harum rem praesentium, in a
+        officia velit. Voluptates velit provident quis tempora et totam ratione
+        eum deleniti accusamus? Quae sint provident incidunt eveniet a deserunt
+        corporis unde, magni itaque doloremque. Voluptatum aliquid neque
+        distinctio odio atque ipsum laboriosam maiores, molestiae dicta
+        accusamus ex porro eum dolorem quaerat enim quam, in aperiam aspernatur
+        commodi tempore. Nemo, accusantium?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+        laboriosam quasi amet ut odit sapiente dolorum id eius enim consequuntur
+        labore, facere itaque. Sequi cumque nostrum eius porro explicabo nihil
+        perspiciatis a corrupti! Doloribus pariatur perferendis, maiores placeat
+        minus eos adipisci iste accusantium totam corporis doloremque fugit
+        deleniti nihil quam quasi numquam soluta, iure vel quos suscipit quidem
+        tempore. Voluptate incidunt facilis consectetur quia cumque harum,
+        itaque, assumenda optio sed eos perspiciatis quasi, earum magnam maxime
+        est nemo. Dolor possimus pariatur ea odit animi quo saepe deserunt nobis
+        reprehenderit nihil optio dolore veritatis voluptas quod, sed voluptatum
+        qui eius quibusdam natus doloribus consectetur soluta ut! Id ipsam
+        excepturi perspiciatis suscipit accusantium debitis sed error est
+        incidunt ipsum reprehenderit iste repudiandae recusandae neque
+        aspernatur eligendi, ullam, magnam molestias? Totam, autem porro a,
+        facilis voluptate ullam quo quam velit est delectus odio? Itaque
+        eligendi quasi repellendus enim facere voluptate laudantium temporibus
+        sapiente. Ea eveniet vero asperiores natus quae, accusamus consequatur
+        ipsum sapiente excepturi autem, iste labore reiciendis laborum illo iure
+        molestiae consequuntur nesciunt blanditiis? Excepturi beatae provident,
+        quibusdam animi, nisi voluptates, neque deserunt alias temporibus
+        tempora atque officia ratione accusamus dicta ut deleniti est
+        consequatur suscipit quod dolore molestias numquam possimus. Ut!
+      </p>
+    </div>
+  );
+};
+
+export default About;
